9ae6f7bc67779ae692b6e4ac02d5b3a1aa665bf2
dilipgunawardene <dilip.kggd@yahoo.com.au>
2019-10-20 13:52:24 +1100

    home page, menu and bootsrap are added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from "@angular/router";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { CarsComponent } from './cars/cars.component';
+import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
+import { CreateCarComponent } from './cars/create-car/create-car.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    CarsComponent,
+    CreateCarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    RouterModule.forRoot([
+      {path: '', component: HomeComponent},
+      {path: 'cars', component: CarsComponent},
+      {path: 'cars/create', component: CreateCarComponent}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
